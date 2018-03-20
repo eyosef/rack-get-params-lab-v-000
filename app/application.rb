64e5ebe1e9b1d32ecbffedaefbf47e2540e37f6c
Eden Mesfun <emesfun@gmail.com>
2018-03-20 14:32:42 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -15,6 +15,9 @@ class Application
     elsif req.path.match(/cart/) && @@cart.empty?
       resp.write "Your cart is empty"
     elsif req.path.match(/cart/) && !@@cart.empty?
+      @@cart.each do |item|
+        resp.write "#{item}\n"
+      end
     elsif req.path.match(/search/)
       search_term = req.params["q"]
       resp.write handle_search(search_term)
